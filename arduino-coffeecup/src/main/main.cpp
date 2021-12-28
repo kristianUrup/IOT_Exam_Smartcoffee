4866da737d2204659245505c4760fd041021fe75
Frederik Jensen <44639732+kungfu15447@users.noreply.github.com>
2021-12-28 15:58:26 +0100

    Clean up code and added reading of ultra sonic sensor

--- arduino-coffeecup/src/main/main.cpp
+++ arduino-coffeecup/src/main/main.cpp
@@ -17,8 +17,7 @@ const char* willMessage = "Frederik er lækker";
 boolean willRetain = false;
 
 OneWire oneWire(TEMP_PIN);
-
-DallasTemperature sensors(&oneWire);
+DallasTemperature temp_sensor(&oneWire);
 
 WiFiClient espClient;
 PubSubClient client(espClient);
@@ -54,23 +53,47 @@ void reconnect() {
 
 void setup() {
   Serial.begin(115200);
+
   setupWifi();
-  pinMode(LED_BUILTIN, HIGH);
   delay(3000);
+
+  pinMode(ULTRASONIC_TRIG_PIN, OUTPUT); // Sets the trigPin as an OUTPUT
+  pinMode(ULTRASONIC_ECHO_PIN, INPUT);
     
   client.setServer(broker, 1883);
-  sensors.begin();
+  temp_sensor.begin();
+}
+
+float getLiquidAmount() {
+  digitalWrite(ULTRASONIC_TRIG_PIN, LOW);
+  delayMicroseconds(2);
+  digitalWrite(ULTRASONIC_TRIG_PIN, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(ULTRASONIC_TRIG_PIN, LOW);
+
+  long duration = pulseIn(ULTRASONIC_ECHO_PIN, HIGH);
+  float distance = duration * SOUND_SPEED / 2;
+
+  return distance;
 }
 
 void loop() {
   if(!client.connected()){
     reconnect();
-  }  
-  sensors.requestTemperatures();
-  float temperatureC = sensors.getTempCByIndex(0);
+  }
+
+  float distance = getLiquidAmount();
+  Serial.print("Distance: ");
+  Serial.print(distance);
+  Serial.println("cm");
+
+  temp_sensor.requestTemperatures();
+  float temperatureC = temp_sensor.getTempCByIndex(0);
   Temperature temp(MACHINE_ID, temperatureC, "Celsius", TEMP_SENSOR_ID);
   String output = temp.ToJsonString();
+
   client.publish(willTopic, output.c_str());
   client.loop();
+
   delay(4000);
 }
